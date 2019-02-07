61a93aaf64d5d48bc9c73c7259372e36e6f44a62
Atif <at454507@dal.ca>
2019-02-07 00:47:50 +0000

    The new config file is now written with the new ip addresses and subnet masks. Only thing that I have not done correctly is string formatiing. I will get back to it after completing other tasks.

--- Model1_task2.py
+++ Model1_task2.py
@@ -2,32 +2,26 @@ def newfile():
 	fout = open('running-config.cfg')
 	fin = open('newconfigfile.cfg','w+')
 
-	a = ''
 	l = []
 	l1 = []
 
 	book = fout.read()
 	line = book.split()
 
-	#for word in line:
-		#l.append(word)
-	#print(l)
+	for i in range(len(line)):
+		if '192.' in line[i]:
+			line[i] = line[i].replace('192.','10.')
+		if '172.' in line[i]:
+			line[i] = line[i].replace('172.','10.')
+		if '255.255.255.0' in line[i]:
+			line[i] = line[i].replace('255.255.255.0','255.0.0.0')
+		if '255.255.0.0' in line[i]:
+			line[i] = line[i].replace('255.255.0.0','255.0.0.0')
 
-	for i in book:
-		for word in line:
-			if '192.' in word:
-				a = word.replace('192.','10.')
-				print(a)
-			if '172.' in word:
-				a = word.replace('172.','10.')
-				print(a)
-			if word == '255.255.0.0':
-				a = word.replace('255.255.0.0','255.0.0.0')
-				print(a)
-			if word == '255.255.255.0':
-				a = word.replace('255.255.255.0','255.0.0.0')
-				print(a)
+	result = '\n'.join(line)
+	print(result)
 
+	fin.write(result)
 
 newfile()
 
